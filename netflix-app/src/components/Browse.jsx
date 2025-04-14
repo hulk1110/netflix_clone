3f231db0204d1ffaf3a47cd12084132aacc5643c
Nishant <nishantsayshellotou@gmail.com>
2025-04-14 13:47:37 +0530

    Header component

--- netflix-app/src/components/Browse.jsx
+++ netflix-app/src/components/Browse.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import { Header } from './Header'
 export const Browse = () => {
   return (
-    <div>Browse</div>
+    <div><Header></Header></div>
   )
 }
---
+++ netflix-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export const Header = () => {
+  return (
+    
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black'>
+        <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="Netflix Logo" className='w-44' />
+    </div>
+  )
+}
