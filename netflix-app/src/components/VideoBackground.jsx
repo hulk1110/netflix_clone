583646b900e6a98b76bd7c5b75b8cf4af8843b51
Nishant <nishantsayshellotou@gmail.com>
2025-04-17 19:23:45 +0530

    main container done

--- netflix-app/src/components/VideoBackground.jsx
+++ netflix-app/src/components/VideoBackground.jsx
@@ -6,13 +6,13 @@ export const VideoBackground = ({ movieId }) => {
   const trailorVideo = useSelector((store) => store.movies.trailorVideo);
 
   return (
-    <div>
+    <div className="w-screen">
       <iframe
+      className="w-screen aspect-video"
         width="560"
         height="315"
-        src={`https://www.youtube.com/embed/` + trailorVideo?.key}
+        src={`https://www.youtube.com/embed/` + trailorVideo?.key +"?autoplay=1&mute=1"}
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
