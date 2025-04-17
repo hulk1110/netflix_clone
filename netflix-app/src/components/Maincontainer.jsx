8fbed74e33bf041c0419ecef270874284b637d00
Nishant <nishantsayshellotou@gmail.com>
2025-04-17 17:37:52 +0530

    Video Tile Component done

--- netflix-app/src/components/Maincontainer.jsx
+++ netflix-app/src/components/Maincontainer.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { VideoTitle } from './VideoTitle'
-import { VideoBackground } from './VideoBackground'
-import { useSelector } from 'react-redux'
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { VideoTitle } from './VideoTitle';
+import { VideoBackground } from './VideoBackground';
 
 export const Maincontainer = () => {
-const movies = useSelector((store)=> store.movies?.nowPlayingMovies);
-if(!movies)return;
-const mainMovie =movies[0];
-console.log(mainMovie);
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+
+  const mainMovie = useMemo(() => {
+    if (!movies || movies.length === 0) return null;
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    return movies[randomIndex];
+  }, [movies]);
+
+  if (!mainMovie) return null;
+
+  const { original_title, overview } = mainMovie;
+
+  console.log(mainMovie);
 
   return (
     <div>
-        <VideoTitle></VideoTitle>
-        <VideoBackground></VideoBackground>
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground />
     </div>
-  )
-}
+  );
+};
